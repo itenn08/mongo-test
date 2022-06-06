3ec8f605f2ba30fafa9a3613b596e5bac80b6e61
Itenn <itenn@ukr.net>
2022-06-06 16:02:00 +0300

    added validation for form

--- ui/src/components/Auth/index.tsx
+++ ui/src/components/Auth/index.tsx
@@ -1,48 +1,104 @@
 import React from "react";
-import {
-  Button,
-  FormControl,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-} from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import { useForm, Controller } from "react-hook-form";
 
 import styles from "./styles.module.scss";
 
+type IFormInput = {
+  email: string;
+  password: string;
+};
+
 const Auth = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  console.log("control", control);
+
+  const onSubmit = (data: IFormInput) => console.log(data);
+
   return (
     <div className={styles["wrapper"]}>
       <div className={styles["title"]}>Log In to Dashboard</div>
       <div className={styles["subTitle"]}>
         Enter your email and password below
       </div>
-      <div className={styles["form"]}>
-        <FormControl variant="outlined" className={styles["input"]}>
-          <InputLabel htmlFor="outlined-adornment-email">
-            Enter your email
-          </InputLabel>
-          <OutlinedInput
-            label="Enter your email"
-            id="outlined-adornment-email"
+      <form onSubmit={handleSubmit(onSubmit)} className={styles["form"]}>
+        <div className={styles["input"]}>
+          <Controller
+            name="email"
+            control={control}
+            defaultValue=""
+            rules={{
+              required: {
+                value: true,
+                message: "Email is required",
+              },
+              pattern: {
+                value:
+                  /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
+                message: "Email is not correct",
+              },
+            }}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <TextField
+                value={value}
+                onChange={onChange}
+                helperText={error ? error.message : null}
+                error={!!error}
+                label="Email"
+                id="outlined-adornment-email"
+                sx={{ width: "250px" }}
+              />
+            )}
           />
-        </FormControl>
-
-        <FormControl variant="outlined" className={styles["input"]}>
-          <InputLabel htmlFor="outlined-adornment-password">
-            Password
-          </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            endAdornment={<InputAdornment position="end"></InputAdornment>}
-            label="Password"
+        </div>
+        <div className={styles["input"]}>
+          <Controller
+            name="password"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <TextField
+                label="Password"
+                value={value}
+                onChange={onChange}
+                error={!!error}
+                helperText={error ? error.message : null}
+                type="password"
+                id="outlined-adornment-password"
+                sx={{ width: "250px" }}
+              />
+            )}
+            rules={{
+              required: {
+                value: true,
+                message: "Email is required",
+              },
+              minLength: {
+                value: 8,
+                message: "Password must be have 8-64 symbols",
+              },
+              maxLength: {
+                value: 64,
+                message: "Password must be have 8-64 symbols",
+              },
+            }}
           />
-        </FormControl>
-      </div>
-      <div className={styles["button"]}>
-        <Button variant="contained" sx={{ width: "250px" }}>
-          Log in
-        </Button>
-      </div>
+        </div>
+        <div className={styles["button"]}>
+          <Button
+            variant="contained"
+            sx={{ width: "250px", height: "50px" }}
+            type="submit"
+          >
+            Log in
+          </Button>
+        </div>
+      </form>
 
       <div className={styles["signUp"]}>
         Don't have an account? <span>Sign up</span>
